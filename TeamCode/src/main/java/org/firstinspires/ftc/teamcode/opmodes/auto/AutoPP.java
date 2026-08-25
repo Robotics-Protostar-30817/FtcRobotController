73e8e10fe0318d6d225b9d2ac9d44b26fd5f56ba
cidy <cidyyy123456@gmail.com>
2026-08-25 15:25:24 -0400

    Pedro installed w/ sample code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoPP.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Robot;
+import dev.nextftc.ftc.NextFTCOpMode;
+public class AutoPP {
+
+}
